53fe8f6a4f4e80bcd365b4360ec5b1136715a0aa
Yazid <yalhujery01@gmail.com>
2022-04-11 22:47:28 -0400

    push

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+function Header(){
+    return (
+        <header>
+            <h3>Yazid Alhujery</h3>
+            <Navigation />
+        </header>
+    )
+}
+
+export default Header
